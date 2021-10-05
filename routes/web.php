95678398a631e4a2c8831ab3d3f2e3d0a47cbf97
Vinicius Piovezan <kiklotplay@gmail.com>
2021-10-05 10:38:35 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::post('/api', [api::class , 'calcular'] );
 
 Route::get('/home', [homeController::class , 'index'] );
