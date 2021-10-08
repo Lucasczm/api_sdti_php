23f3f6efdb5f3f9c6a4894acada47742d443d92a
Vinicius Piovezan <kiklotplay@gmail.com>
2021-10-07 21:40:05 -0300

    v4

--- app/Http/Controllers/configController.php
+++ app/Http/Controllers/configController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\app_config;
 use App\Models\responsaveis_conselhos;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -10,33 +11,63 @@ class configController extends Controller
 {
     public function index(Request $req)     
     {
-
+        $config = app_config::get();
         $mensagem = $req->session()->get('mensagem');
         return view('config.index' , 
-        ['mensagem'=> $mensagem]);
+        ['mensagem'=> $mensagem,
+         'configuracoes' => $config,
+        'raio' => $config[0]->raio_espaco]);
     }
 
     public function store(Request $req)
-    {   
-        DB::beginTransaction();
-        $resp = responsaveis_conselhos::create([
-            "nome" => $req->nomeResp,
-            "email" => $req->emailResp,
-            "tel" => $req->telefoneResp,
-            "endereco" => $req->enderecoCon,
-        ]);
-
-        $resp->conselhos()->create([
-            "lat" => $req->lat,
-            "lon" => $req->lon,
-            "email_con" => $req->emailCon,
-            "nome_con" => $req->nomeCon,
-            "tel_con" => $req->telefoneCon,
-            "endereco_con" => $req->enderecoCon,
-        ]);
-        DB::commit();
+    {       
         
-        $req->session()->flash('mensagem' , "Conselho Nº $resp->id criado com sucesso!");
-        return redirect()->route('configuracoes');
+        if($req->tipo_form == "conselho"){
+
+            DB::beginTransaction();
+            $resp = responsaveis_conselhos::create([
+                "nome" => $req->nomeResp,
+                "email" => $req->emailResp,
+                "tel" => $req->telefoneResp,
+                "endereco" => $req->enderecoCon,
+            ]);
+
+            $resp->conselhos()->create([
+                "lat" => $req->lat,
+                "lon" => $req->lon,
+                "email" => $req->emailCon,
+                "nome" => $req->nomeCon,
+                "tel" => $req->telefoneCon,
+                "endereco" => $req->enderecoCon,
+            ]);
+            DB::commit();
+            
+            $req->session()->flash('mensagem' , "Conselho Nº $resp->id criado com sucesso!");
+            return redirect()->route('configuracoes');
+        }
+        elseif($req->tipo_form == "raio")
+        {
+            DB::beginTransaction();
+
+            $raio = app_config::get();
+            $raio[0]->raio_espaco = $req->valor;
+            $raio[0]->save();
+            DB::commit();
+            $req->session()->flash('mensagem' , "Valor do raio de circulos alterado com sucesso!");
+
+
+        }
+        elseif($req->tipo_form == "trigger")
+        {
+            DB::beginTransaction();
+
+            $trigger = app_config::get();
+            $trigger[0]->trigger_denuncias = $req->valor;
+            $trigger[0]->save();
+            DB::commit();
+            $req->session()->flash('mensagem' , "Valor de trigger alterado com sucesso!");
+
+
+        }
     }
 }
--- resources/views/config/index.blade.php
+++ resources/views/config/index.blade.php
@@ -22,6 +22,7 @@ Configurações API TBR
     <div class="container mb-5 mt-2">
         <div class="row">
             <div class="col col-5 ">
+                <input type="text" name="tipo_form" id="tipo_form" hidden value="conselho" > 
                 <label for="nome" class="form-label">*Nome conselho:</label>
                 <input type="text" class="form-control" id="nomeCon"   name="nomeCon" value="{{ old('nomeCon') }}">
             </div>
@@ -81,5 +82,145 @@ Configurações API TBR
         <button class="btn btn-success" type="submit" id="enviar">Cadastrar</button>
 </form>
 
+<h3  class="mt-5 mb-4">Configurações de funcionamento:</h3>
+
+<div class="container mb-5">
+    
+    <ul class="list-group">
+        <div class="col col-6 ">
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            Raio:
+            <span id="raio">{{$configuracoes[0]->raio_espaco}}</span>
+
+            <div class="input-group w-55" hidden id="input_raio">   
+                <input type="text" class="form-control" value="" id="input_raio_valor">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" onclick="confirmarEdicao('raio')" >
+                            <i class="fas fa-check"></i>
+                        </button>
+                        @csrf
+                    </div>
+            </div>
+
+            <span class="d-flex">
+                <button class="btn btn-info btn-sm me-2" onclick="editar('raio')" id="btn_editar_raio">
+                    <i class="fas fa-edit"></i>
+                </button>
+
+            </span> 
+        </li>
+
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            Trigger:
+            <span id="trigger">{{$configuracoes[0]->trigger_denuncias}}</span>
+
+            <div class="input-group w-55" hidden id="input_trigger">   
+                <input type="text" class="form-control" value="" id="input_trigger_valor">
+                    <div class="input-group-append">
+                        <button class="btn btn-primary" onclick="confirmarEdicao('trigger')" >
+                            <i class="fas fa-check"></i>
+                        </button>
+                        @csrf
+                    </div>
+            </div>
+
+            <span class="d-flex">
+                <button class="btn btn-info btn-sm me-2" onclick="editar('trigger')" id="btn_editar_trigger">
+                    <i class="fas fa-edit"></i>
+                </button>
+
+            </span> 
+        </li>
+    
+    </ul>
+</div>
+
+<script>
+
+    raio_ = document.getElementById(`raio`).textContent;
+    trigger_ = document.getElementById(`trigger`).textContent;
+
+function editar(tipo)
+    {
+        console.log(tipo);
+        switch(tipo)
+        {
+            case 'raio':
+                document.getElementById('raio').hidden = true;
+                document.getElementById('btn_editar_raio').hidden = true;
+                document.getElementById('input_raio_valor').value = raio_;
+                document.getElementById('input_raio').removeAttribute('hidden');
+            break;
+
+            case 'trigger':
+
+                document.getElementById('trigger').hidden = true;
+                document.getElementById('btn_editar_trigger').hidden = true;
+                document.getElementById('input_trigger_valor').value =  trigger_;
+                document.getElementById('input_trigger').removeAttribute('hidden');
+
+
+            break;
+        }
+    }
+
+    function confirmarEdicao(tipo)
+    {
+
+        switch(tipo)
+        {
+            case 'raio':
+                valor = document.getElementById('input_raio_valor').value;
+                if(raio_ == valor){
+
+                    document.getElementById('raio').removeAttribute('hidden');
+                    document.getElementById('btn_editar_raio').removeAttribute('hidden');
+                    document.getElementById('input_raio').hidden = true;
+                }
+                else{
+                    let formData = new FormData(); //Vai ser um fomulario enviado pelo javascript
+                    formData.append('valor', valor); 
+                    formData.append('tipo_form', 'raio'); 
+                    formData.append('_token', document.querySelector(`input[name="_token"]`).value); 
+
+                    fetch(document.URL, {
+                    method: 'POST', 
+                    body: formData
+                    }).then(() => {
+                    document.location.reload(true);
+                    });
+                }
+            break;
+
+            case 'trigger':
+
+            valor = document.getElementById('input_trigger_valor').value;
+                if(trigger_ == valor){
+
+                    document.getElementById('trigger').removeAttribute('hidden');
+                    document.getElementById('btn_editar_trigger').removeAttribute('hidden');
+                    document.getElementById('input_trigger').hidden = true;
+                }
+                else{
+                    let formData = new FormData(); //Vai ser um fomulario enviado pelo javascript
+                    formData.append('valor', valor); 
+                    formData.append('tipo_form', 'trigger'); 
+                    formData.append('_token', document.querySelector(`input[name="_token"]`).value); 
+
+                    fetch(document.URL, {
+                    method: 'POST', 
+                    body: formData
+                    }).then(() => {
+                    document.location.reload(true);
+                    });
+                }
+
+
+            break;
+        }
+
+    }
+
+</script>
 
 @endsection
